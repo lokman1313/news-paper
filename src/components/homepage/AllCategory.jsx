cc6c9da737c72e557ad449d9c797926b6b7b4c49
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-05-04 22:47:12 +0600

    some error handel

--- src/components/homepage/AllCategory.jsx
+++ src/components/homepage/AllCategory.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const AllCategory = ({categorys,activeId}) => {
     return (
-         <ul className="flex flex-col gap-2 justify-center">
+         <ul className="flex flex-col gap-2 justify-center p-2">
             {
                 categorys.map(category => <li key={category.category_id} className={`${activeId === category.category_id ? "bg-purple-400": ""} p-2 text-center font-semibold text-md rounded-md`}>
                     <Link href={`/category/${category.category_id}`} className="block">
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,26 +1,23 @@
 "use client"
 import Link from "next/link";
-import user from '@/assets/user.png'
-import Image from "next/image";
 import NavLink from "./NavLink";
 import { authClient } from "@/app/lib/auth-client";
-import { redirect } from "next/navigation";
 
 const Navbar = () => {
     const { data: session,isPending } = authClient.useSession()
     const userData = session?.user;
     console.log(userData)
     return (
-        <div className="flex justify-between items-center">
-            <div></div>
+        <div className="flex justify-between items-center px-2">
+            
             <ul className="flex justify-center items-center gap-3">
                 <li><NavLink href={'/'}>Home</NavLink></li>
                 <li><NavLink href={'/about'}>About</NavLink></li>
                 <li><NavLink href={'/career'}>Career</NavLink></li>
             </ul>
-            { isPending ? <h2>Loding.....</h2> : userData ?  (<div className="flex justify-between items-center gap-2">
-                <p>Hello {userData?.name}</p>
-                <Image src={user} alt="user" width={20} ></Image>
+            { isPending ? <h2>Loding.....</h2> : userData ?  (<div className="flex justify-between items-center sm:gap-2">
+                <p>{userData?.name}</p>
+                
                 <button className="btn" onClick={ async ()=> await authClient.signOut() }>Logout</button>
             </div>)
             : 
